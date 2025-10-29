7205cd4cece883d84f1c3441ce63ce0ed0bd1791
vitoCorl1 <elhkadrawiaymane@gmail.com>
2025-10-29 14:31:42 +0100

    Create an array/object structure for reservations.

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 const overlay = document.getElementById('overlay');
 
 const days = document.getElementById('wrap-days');
-const day_children = days.querySelectorAll('*');
+const day_children = days.querySelectorAll('div');
 
 const closeBtn = document.getElementById('closeBtn');
 const dialog = document.getElementById('dialog');
 
-const dataReservation = {};
+const allData = {};
 
-
+let selectedDay = null;
 day_children.forEach(element => {
     element.addEventListener('click', () => {
+        const dayNumber = element.querySelector('span')?.textContent.trim();
+        if (!dayNumber) return;
+        selectedDay = dayNumber;
         overlay.classList.remove("hidden");
         dialog.focus();
     });
 });
 
+
 const res_title = document.getElementById('res-title');
-res_title.addEventListener("input", () => {
-    console.log(res_title.value);
-})
-
 const num_people = document.getElementById('num-people');
-num_people.addEventListener("input", () => {
-    console.log(num_people.value)
-})
-
 const start_time = document.getElementById('start-time');
-
+const end_time = document.getElementById('end-time');
+const reservation_type = document.getElementById('reservation-type');
 
 closeBtn.addEventListener("click", () => {
     overlay.classList.add("hidden");
 });
 
+const ConfirmBtn = document.getElementById('ConfirmBtn');
+ConfirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if((res_title.value).length < 5){
+        console.error("wrong nane input");
+        res_title.style.border = "1px solid red";
+        return;
+    }
+    
+    if (!allData[selectedDay]) allData[selectedDay] = [];
+    allData[selectedDay].push({
+        name: res_title.value,
+        people: num_people.value,
+        start: start_time.value,
+        end: end_time.value
+    })
+    console.log(allData);
+    overlay.classList.add('hidden')
+})
 
